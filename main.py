2b41abfe6e5c3877da5868bf8e110258b8b4e4e7
valeria <16-11233@usb.ve>
2023-07-19 10:54:21 -0400

    fixed

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     chosen_algorithm = input("Insert a number to pick one of the algorithms: ")
 
     chosen_depth = int(input("\nInsert a number to pick the maximum depth the algorithm will go to: "))
+    print()
 
     game = connect4.connect_4()
     computer_player = 'R'
@@ -33,9 +34,10 @@ if __name__ == '__main__':
             move_to_make = algorithms.negamax(game, chosen_depth, computer_player)
         elif chosen_algorithm == '1':
             move_to_make = algorithms.negamax_alpha_beta(game, chosen_depth, -200, 200, computer_player)
-        R_time_end = time.time()   
+        R_time_end = time.time()
 
-        print(f'R\'s Thinking time: {round(R_time_end - R_time_start, 2)} seconds\n') 
+        print(f'R\'s Thinking time: {round(R_time_end - R_time_start, 2)} seconds') 
+        print(f'R\'s move: {move_to_make}\n')
 
         game = game.make_move(computer_player, move_to_make)
         game.print_board()
@@ -56,6 +58,7 @@ if __name__ == '__main__':
             Y_time_end = time.time()  
 
             print(f'Y\'s Thinking time: {round(Y_time_end - Y_time_start, 2)} seconds')
+            print(f'Y\'s move: {move_to_make}\n')
 
         elif chosen_modality == '1':
             move_to_make = int(input(f'Choose a column to move to = {game.valid_moves()}: '))    
